0abd79588f47d0111f327eef44d9a4d3c7d2e08a
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-16 08:36:38 +0300

    Added the API url using the fetch method to retrieve data from the API

--- src/components/events.js
+++ src/components/events.js
@@ -15,6 +15,14 @@ const Events = () => {
   return (
     <div>
         <p>Feeling bored? <br/> Here are some fun activities to engage in:</p>
+    <div>
+        <ul>
+            <li>{activities.type}</li>
+            <li>{activities.activity}</li>
+            <li>{activities.participants}</li>
+
+        </ul>
+    </div>
     </div>
   )
 }
