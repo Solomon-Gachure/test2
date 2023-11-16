35110d8f26b247ec4f9e81772a3aa4baf19fe528
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-16 08:01:46 +0300

    Added the API url and used the fetch method to get the data

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FetchData = () => {
+
+    const [joke, setJoke]=useState({})
+    useEffect(()=>{
+        fetch('https://official-joke-api.appspot.com/random_joke')
+        .then(response => response.json())
+        .then(data => setJoke (data))
+        .catch(err => console.log(err))
+    }, [])
+
   return (
-    <div>FetchData</div>
+    <div>
+        <p>Hey there, this is a web app for programming jokes.</p>
+        <div>
+            <ul>
+                <li>{joke.setup}</li>
+                <li>{joke.punchline}</li>
+   </ul>
+        </div>
+    </div>
   )
 }
 
