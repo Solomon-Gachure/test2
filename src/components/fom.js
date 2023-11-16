4439130f0afa412601815d9549be86706f7eba9e
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-16 13:06:53 +0300

    Fetched the API using axios and it was successful

--- src/components/fom.js
+++ src/components/fom.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Fom = () => {
+const[fom,setFom]=useState({})
+useEffect(()=>{
+    axios.get('https://www.boredapi.com/api/activity')
+    .then(res => setFom(res.data))
+    .catch(err => console.log(err))
+})
+
+
   return (
     <div>
-        
+        <p>Feeling bored? <br/> Here are some fun activities to engage in:</p>
+    <div>
+        <ul>
+            <li>{fom.type}</li>
+            <li>{fom.activity}</li>
+            <li>{fom.participants}</li>
+
+        </ul>
+    </div>
     </div>
   )
 }
