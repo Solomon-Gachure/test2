8bf500934549329702365cab563454104e702fd1
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-16 08:17:31 +0300

    Created the cats facts page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import FetchData from "./components/FetchData";
+import Cats from "./components/cats";
 
 
 function App() {
   return (
     <div className="App">
       <FetchData/>
+      <Cats/>
     </div>
   );
 }
---
+++ src/components/cats.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+const Cats = () => {
+ 
+    const [cats,setCats]=useState({})
+    useEffect(()=>{
+        
+    })
+
+
+
+  return (
+    <div>
+        <p>Get right facts about cats</p>
+    </div>
+  )
+}
+
+export default Cats
