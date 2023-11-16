654715990abbee26a3d00c878e20ff81baf6decc
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-16 12:31:17 +0300

    Created the mbwa page and fetched as well the API using axios

--- src/components/mbwa.js
+++ src/components/mbwa.js
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Mbwa = () => {
+const [mbwa,setMbwa]=useState({})
+useEffect(()=>{
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(res=> (setMbwa(res.data)))
+    .catch(err => console.log(err))
+}, [])
   return (
-    <div>Mbwa</div>
+    <div>
+        
+    </div>
   )
 }
 
